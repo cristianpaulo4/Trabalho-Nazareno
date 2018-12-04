204f8d4817eff4402a14248562ef5a646d87cd27
bx <bx@bx-PC>
2018-12-03 23:49:22 -0300

    Primeiro Projeto - Cristian

--- src/Interfaces/Home.java
+++ src/Interfaces/Home.java
@@ -21,8 +21,8 @@ public class Home extends javax.swing.JFrame {
         initComponents();
         
         this.setTitle("Sistema de Vendas");
-        
-        
+        this.setIconImage(null);
+       
         
     }
 
