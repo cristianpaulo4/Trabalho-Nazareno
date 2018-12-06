48bd78a520d10464dfc51e4a5abaf11536e7a100
bx <bx@bx-PC>
2018-12-06 12:01:26 -0300

    Endereço e produto criados

---
+++ src/Objetos/Produto.java
@@ -0,0 +1,132 @@
+
+package Objetos;
+
+public class Produto {
+    
+    private int idProduto;
+    private String nome;
+    private int quantidade;
+    private int quant_minimo;
+    private String validade;
+    private double valor_venda;
+    private double valor_custo;
+    private int idFornecedor;
+
+    
+    // METODOS DE INSERÇÃO
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getQuant_minimo() {
+        return quant_minimo;
+    }
+
+    public void setQuant_minimo(int quant_minimo) {
+        this.quant_minimo = quant_minimo;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+
+    public double getValor_venda() {
+        return valor_venda;
+    }
+
+    public void setValor_venda(double valor_venda) {
+        this.valor_venda = valor_venda;
+    }
+
+    public double getValor_custo() {
+        return valor_custo;
+    }
+
+    public void setValor_custo(double valor_custo) {
+        this.valor_custo = valor_custo;
+    }
+
+    public int getIdFornecedor() {
+        return idFornecedor;
+    }
+
+    public void setIdFornecedor(int idFornecedor) {
+        this.idFornecedor = idFornecedor;
+    }
+    
+    
+    
+    
+    // METODOS CRUD
+    
+    
+    // CADASTRAR
+    public void Cadastrar(Produto produto){
+        
+        
+        
+    }
+    
+    
+    // Alterar
+    public void Alterar(Produto produto){
+        
+        
+        
+    }
+    
+    
+    // Listar
+    public void Listar (){
+        
+        
+        
+        
+    }
+    
+    // Pesquisar
+    public void Pesquisar (int  codigo){
+        
+        
+        
+        
+    }
+    
+    
+    // Excluir
+    public void Excluir(int  codigo){
+        
+        
+        
+                
+        
+    }
+    
+    
+        
+    
+}
