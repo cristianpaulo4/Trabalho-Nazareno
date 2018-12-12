71347f9918a2c56424e3e560858bb9a808d20ba3
Willam <Willam@10.0.0.108>
2018-12-12 13:25:53 -0200

    Cristian

---
+++ src/Objetos/Fornecedor.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author Willam
+ */
+public class Fornecedor extends Endereco{
+     private int cnpj;
+    private String nome;
+    private String telefone;
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public void salvar(){
+    
+    }
+    
+}
+
+
