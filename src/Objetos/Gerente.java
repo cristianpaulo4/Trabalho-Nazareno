b90e5d17c0b6b177cf7f6f4622098043895dc8bb
aluno <aluno@localhost>
2018-12-12 19:04:49 -0300

    wanderson

---
+++ src/Objetos/Gerente.java
@@ -0,0 +1,55 @@
+
+package Objetos;
+
+
+public class Gerente extends Endereco{
+    
+    private int ID_gerente;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private double salario;
+
+    public int getID_gerente() {
+        return ID_gerente;
+    }
+
+    public void setID_gerente(int ID_gerente) {
+        this.ID_gerente = ID_gerente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+    
+}
