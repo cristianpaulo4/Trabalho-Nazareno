fc50251d1ca314277dd55931a3a85e6c16697b2e
aluno <aluno@localhost>
2018-12-12 21:37:46 -0300

    wanderson

--- src/BDconexao/conexao.java
+++ src/BDconexao/conexao.java
@@ -5,11 +5,17 @@ import java.sql.*;
 
 
 public class conexao {
-    
+ /*   
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://trabalho-ifpi.mysql.uhserver.com/trabalho_ifpi";
     private static final String USER = "farmacia_ifpi";
     private static final String PASS = "ifpi@010203";    
+  */   
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3305/trabalho_ifpi";
+    private static final String USER = "root";
+    private static final String PASS = "";    
+    
     
     
     public  Connection getConnection(){
