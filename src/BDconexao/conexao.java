14c5d144b454c9d0c73ae3b6b8f81ff241e7fb32
aluno <aluno@localhost>
2018-12-12 23:19:55 -0300

    Cristian

--- src/BDconexao/conexao.java
+++ src/BDconexao/conexao.java
@@ -1,32 +1,28 @@
-
 package BDconexao;
 
 import java.sql.*;
 import javax.swing.JOptionPane;
 
-
 public class conexao {
-    
-     private static final String DRIVER = "com.mysql.jdbc.Driver";
-    /*private static final String URL = "jdbc:mysql://trabalho-ifpi.mysql.uhserver.com/trabalho_ifpi";
+
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://trabalho-ifpi.mysql.uhserver.com/trabalho_ifpi";
     private static final String USER = "farmacia_ifpi";
-    private static final String PASS = "ifpi@010203";    
-   */
-    
-    
-    private final String URL = "jdbc:mysql://localhost:3305/trabalho_ifpi";
+    private static final String PASS = "ifpi@010203";
+
+    /* private final String URL = "jdbc:mysql://localhost:3305/trabalho_ifpi";
     private final String USER = "root";
     private final String PASS = "";
-    
-    public  Connection getConnection(){
+     */
+    public Connection getConnection() {
         try {
-            Class.forName(DRIVER); 
-           // JOptionPane.showMessageDialog(null, "conectado");
+            Class.forName(DRIVER);
+            // JOptionPane.showMessageDialog(null, "conectado");
             return DriverManager.getConnection(URL, USER, PASS);
-            
+
         } catch (ClassNotFoundException | SQLException ex) {
-           throw new RuntimeException("Erro na conexao", ex);
-           
-        }   
+            throw new RuntimeException("Erro na conexao", ex);
+
+        }
     }
 }
